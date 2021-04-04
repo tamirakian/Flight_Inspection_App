dfbbada9e587366f273ff1b75f0e6151ec7c4239
Gabriel Cembal <gabriel.cembal@live.biu.ac.il>
2021-04-04 16:08:26 +0300

    4.4.2020 gaby 16:08

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,26 +21,9 @@ namespace Flight_Inspection_App
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Control currentUser;
-        private MediaPlayerController mpc;
-        private DataModel dm;
-        public Socket client;
         public MainWindow()
         {
-            InitializeComponent();
-            client = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
-            dm = new DataModel();
-            mpc = new MediaPlayerController();
-            currentUser = mpc;
-            myStack.Children.Add(currentUser);
-        }
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            myStack.Children.Clear();
-            if(currentUser == mpc)
-            {
-                myStack.Children.Add(currentUser); 
-            }
+            InitializeComponent();     
         }
     }
 }
